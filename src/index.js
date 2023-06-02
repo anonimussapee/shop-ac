2e092afa0aeaee4fcc1396cb35ec1db6a3657d89
Javier Ospina <jaheosig10@gmail.com>
2023-06-02 01:39:45 -0500

    tarjetas de productos

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-
 const productContainer = document.getElementById('product-container');
 
 fetch('https://ecommercebackend.fundamentos-29.repl.co')
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(product => {
-      const productElement = document.createElement('div');
-      productElement.innerHTML = `
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(product => {
+            const productElement = document.createElement('div');
+            productElement.classList.add('product-card');
+            productElement.innerHTML = `
         <img src="${product.image}" alt="${product.name}">
         <h3>${product.name}</h3>
         <p>${product.description}</p>
         <span>Precio: $${product.price}</span>
       `;
-      productContainer.appendChild(productElement);
-    });
-  })
-  .catch(error => console.error(error));
+            productContainer.appendChild(productElement);
+        });
+    })
+    .catch(error => console.error(error));
