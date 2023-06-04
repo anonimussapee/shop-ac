5643274f1afb231c97ec3a76ec1a2cd81302d9d6
Javier Ospina <jaheosig10@gmail.com>
2023-06-04 11:47:08 -0500

    almost finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* API */
 const productContainer = document.getElementById('product-container');
 
 fetch('https://ecommercebackend.fundamentos-29.repl.co')
@@ -7,11 +8,16 @@ fetch('https://ecommercebackend.fundamentos-29.repl.co')
             const productElement = document.createElement('div');
             productElement.classList.add('product-card');
             productElement.innerHTML = `
-        <img src="${product.image}" alt="${product.name}">
-        <h3>${product.name}</h3>
-        <p>${product.description}</p>
-        <span>Precio: $${product.price}</span>
-      `;
+            <img src="${product.image}" alt="${product.name}">
+             <div class="product-info">
+                <div class="name">${product.name}</div>
+                <div class="price">$${product.price}</div>
+             </div>
+            <p>${product.description}</p>
+                <div class="additional-info">
+                    <p class="category">Categoría: ${product.category}</p>
+                    <p class="quantity">Cantidad: ${product.quantity}</p>
+                </div>`;
             productContainer.appendChild(productElement);
         });
     })
